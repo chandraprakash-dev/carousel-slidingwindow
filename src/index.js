acce6fe7a0ac730c83e36e23e7ebf1a9a5d15364
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-02-18 21:20:52 +0530

    Change scroll functionality to use transform (improves performance)

--- src/index.js
+++ src/index.js
@@ -1,33 +1,26 @@
-const carousel = document.querySelector('#carousel')
-/* label the images, just for convenience, to visually track them */
-let i = 1;
-for(const li of carousel.querySelectorAll('li')) {
-  li.style.position = 'relative';
-  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-  i++;
+const carousel = document.querySelector('.carousel');
+const images = document.querySelector('.images');
+
+const prev = carousel.querySelector('.prev');
+const next = carousel.querySelector('.next');
+
+const width = 150;
+const count = 3;
+let position = 0;
+
+function scrollLeft() {
+  position += count;
+  console.log(position);
+  const off = position * width;
+  images.style.transform = `translateX(-${off}px)`;
 }
 
-/* configuration */
-const width = 130; // image width
-const count = 3; // visible images count
+function scrollRight() {
+  position -= count;
+  console.log(position);
+  const off = position * width;
+  images.style.transform = `translateX(-${off}px)`;
+}
 
-const list = carousel.querySelector('ul');
-const listElems = carousel.querySelectorAll('li');
-
-let position = 0; // ribbon scroll position
-
-carousel.querySelector('.prev').onclick = function() {
-  // shift left
-  position += width * count;
-  // can't move to the left too much, end of images
-  position = Math.min(position, 0)
-  list.style.marginLeft = `${position  }px`;
-};
-
-carousel.querySelector('.next').onclick = function() {
-  // shift right
-  position -= width * count;
-  // can only shift the ribbbon for (total ribbon length - visible count) images
-  position = Math.max(position, -width * (listElems.length - count));
-  list.style.marginLeft = `${position  }px`;
-};
+prev.addEventListener('click', scrollRight);
+next.addEventListener('click', scrollLeft);
